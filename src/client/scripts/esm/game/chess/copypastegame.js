b3faa98cf5f870af41f81ba957bea9881033eebe
Mmdrlol <104565100+Mmdrlol@users.noreply.github.com>
2025-02-11 14:17:44 +0100

    paste a position without the prior moves

--- src/client/scripts/esm/game/chess/copypastegame.js
+++ src/client/scripts/esm/game/chess/copypastegame.js
@@ -26,7 +26,7 @@ import gameloader from './gameloader.js';
  */
 
 /** Enable to only copy a single position without all the moves prior */
-const copySinglePosition = false; 
+const copySinglePosition = true; 
 
 /**
  * A list of metadata properties that are retained from the current game when pasting an external game.
